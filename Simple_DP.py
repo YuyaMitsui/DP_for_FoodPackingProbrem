8d9a7e84e92ed2bee92338bc14e3d41ebdaa9361
yuya mistui <copcop77@gmail.com>
2020-12-14 01:05:47 +0900

    Lexico_DP完成

--- Simple_DP.py
+++ Simple_DP.py
@@ -45,9 +45,9 @@ def Lexico_DP(w,priority,T):
     w_max = max(w)
 
     #行列数設定
-    num_rows = T + w_max
+    # num_rows = T + w_max
     num_columns = n
-
+    num_rows = min(T + w_max - 1,W)
     #DPを行う二次元配列初期化
     #y:重さのDP表
     #z:優先度のDP表
@@ -55,8 +55,8 @@ def Lexico_DP(w,priority,T):
 
     DP_LIST_y = [[0 for i in range(num_columns)] for j in range(num_rows)]
     DP_LIST_z = [[-1 for i in range(num_columns)] for j in range(num_rows)]
-    priority_u = [[0 for i in range(num_columns)] for j in range(num_rows)]
-    priority_v = [[0 for i in range(num_columns)] for j in range(num_rows)]
+    priority_u = [[-1 for i in range(num_columns)] for j in range(num_rows)]
+    priority_v = [[-1 for i in range(num_columns)] for j in range(num_rows)]
 
     DP_LIST_y[0] = [1] * num_columns
     DP_LIST_z[0] = [0] * num_columns
@@ -66,7 +66,7 @@ def Lexico_DP(w,priority,T):
         for p in range(1,num_rows):
             if k == 0 and p == w[0]:
                 DP_LIST_y[p][0] = 1
-                priority_u[p][k] = priority[k]
+                priority_v[p][k] = priority[k]
             else:
                 if DP_LIST_y[p][k-1] == 1:
                     DP_LIST_y[p][k] = 1
@@ -83,22 +83,17 @@ def Lexico_DP(w,priority,T):
         if DP_LIST_y[i][num_columns-1] == 1: #最終列の目的重量を満たす解を見つける
             ansIDX = i
             break
-
+    
     #バックトラック
-    x = [-1 for i in range(n)]
+    x = [0 for i in range(n)]
     searchIDX = ansIDX
     for i in reversed(range(0,n)):
-        if DP_LIST_y[searchIDX][i-1] == 1:
-            x[i] = 0
-        if DP_LIST_y[searchIDX - w[i]][i-1] == 1:
-            x[i] = 1
-        if DP_LIST_y[searchIDX][i-1] == 1 and DP_LIST_y[searchIDX - w[i]][i-1] == 1:
+        if searchIDX >= w[i]:    
             if priority_v[searchIDX][i] >= priority_u[searchIDX][i]:        
                 x[i] = 1
                 searchIDX -= w[i]
             else:
                 x[i] = 0
-
     #debug 
     # for j in range(num_rows):
     #     for i in range(num_columns):
@@ -106,6 +101,7 @@ def Lexico_DP(w,priority,T):
     #         if(i != num_columns-1):
     #             print(" ",end ='')
     #     print(" \n")
+        
     # for j in range(num_rows):
     #     for i in range(num_columns):
     #         print(DP_LIST_z[j][i],end ='')
@@ -119,6 +115,7 @@ def Lexico_DP(w,priority,T):
 #main
 w = [3,7,5,8,2] #重さリスト
 priority = [5,5,1,1,3] #優先度リスト
-T = 10 #目的重量
+T = 9 #目的重量
 # x = Simple_DP(w,T)
 x = Lexico_DP(w,priority,T)
+print(x)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+      # if DP_LIST_y[searchIDX - w[i]][i-1] == 1:
+            #     x[i] = 1
+            #     searchIDX -= w[i]
+            # if DP_LIST_y[searchIDX][i-1] == 1 and DP_LIST_y[searchIDX - w[i]][i-1] == 1:
+            #     if priority_v[searchIDX][i] >= priority_u[searchIDX][i]:        
+            #         x[i] = 1
+            #         searchIDX -= w[i]
+            # if priority_v[searchIDX][i] >= priority_u[searchIDX][i]:        
+            #         x[i] = 1
+            #         searchIDX -= w[i]
